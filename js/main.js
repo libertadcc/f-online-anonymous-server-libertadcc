21c2626cefdc2d5aa1f6850fcf1274eac43b090d
libertadcc <libertadchapinalcervantes@gmail.com>
2019-07-04 20:38:34 +0200

    Add functionality

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const navTrigger = document.querySelector('.nav-trigger');
+const nav = document.querySelector('.nav');
+const navClose = document.querySelector('.nav-close');
+
+const show = () => {
+  nav.classList.remove('hidden');
+  navTrigger.classList.add('hidden');
+}
+
+const hide = () => {
+  nav.classList.add('hidden');
+  navTrigger.classList.remove('hidden');
+}
+
+navTrigger.addEventListener('click', show);
+navClose.addEventListener('click', hide);
